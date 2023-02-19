826e49358a8f533da59af9451bfc1657c7560b0c
AboodDrago <112480539+AboodDrago@users.noreply.github.com>
2023-02-19 01:58:40 +0100

    First Commit

---
+++ style.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Medical Student", "Frontend Developer", "Web Developer", "YouTuber"],
+    typeSpeed: 90,
+    backSpeed: 100,
+    startDelay: 1800,
+    backDelay: 1000,
+    loop: true,
+})
